940e0ea0efece406a486ba13a0f5fa14b8599686
Kevin Michael <biobot.mike@gmail.com>
2019-12-01 08:49:11 +0200

    Add getItems method in ItemCtrl. Add populateItemList method in UICtrl & UISelectors object.

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ const ItemCtrl = (function() {
   }
   // Public methods
   return {
+    getItems: function() {
+      return data.items;
+    },
     logData: function() {
       return data;
     }
@@ -32,9 +35,29 @@ const ItemCtrl = (function() {
 
 // UI Controller
 const UICtrl = (function() {
+  const UISelectors = {
+    itemList: 'item-list'
+  }
   // Public methods
   return {
+    populateItemList: function(items) {
+      let html = '';
 
+      items.forEach(item => {
+        html += `
+          <li class="collection-item" id="item-${item.id}">
+            <strong>${item.name}: </strong> <em>${item.calories} cal</em>
+
+            <a class="secondary-content" href="#">
+              <i class="fa fa-pencil edit-item"></i>
+            </a>
+          </li>
+        `;
+      });
+
+      // Insert list items
+      document.getElementById(UISelectors.itemList).innerHTML = html;
+    }
   }
 })();
 
@@ -44,7 +67,10 @@ const AppCtrl = (function(ItemCtrl, UICtrl) {
   // Public methods
   return {
     init: function() {
-      console.log('Initializing App...');      
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+      // Populate list with items
+      UICtrl.populateItemList(items);
     }
   }
 })(ItemCtrl, UICtrl);
